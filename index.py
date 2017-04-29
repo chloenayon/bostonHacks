712a29ec95de7b8a7eb98c6bd2efbd14565ff091
Chloe Kaubisch <nemononiam@gmail.com>
2017-04-29 18:27:36 -0400

    no progress

--- index.py
+++ index.py
@@ -2,19 +2,28 @@ from PIL import Image, ImageFilter
 import numpy as np
 import random
 import math
-
+from numpy import array
 
 HIDDEN_LAYERS_SIZE = 100
+OUTPUT_SIZE = 26
 
 im = Image.open('helloWorld.png')
+input = np.asarray(im)
+
+print input.size
+
+INPUT_SIZE = input.size
+
+#print input.length
+
 #pixels = list(im.getdata())
-width, height = im.size
+#width, height = im.size
 
-
+#print pixels
 
 #b = im.load()
 
-im = np.asarray(Image.open('helloWorld.png').convert('L'))
+#im = np.asarray(Image.open('helloWorld.png').convert('L'))
 
 def sigmoid(x):
     return 1/(1 + math.exp(-x))
@@ -24,15 +33,29 @@ def activation(arr, rows, cols):
         for m in range(cols):
             arr[n, m] = sigmoid(arr[n,m])
 
-######################## FORWARD PROPAGATION ##############################
 
-######################## LAYER ONE #############################
+######################## INPUT LAYER ######################################
 
-layerOne = np.random.random((width,HIDDEN_LAYERS_SIZE))
+weights_0 = np.random.random((100, INPUT_SIZE))
+bias_0 = np.random.rand(INPUT_SIZE)
 
-hiddenLayer = np.dot(im, layerOne)
+hiddenLayer = weights_0.dot(input) + bias_0
 
-activation(hiddenLayer, height, HIDDEN_LAYERS_SIZE)
+#print hiddenLayer
+#activation(hiddenLayer, height, HIDDEN_LAYERS_SIZE)
+
+########################## HIDDEN LAYER ###################################
+
+
+    
+
+###########################################################################
+
+
+
+
+
+####################### LAST STEP #########################################
 
 
 
